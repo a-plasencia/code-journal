278fc8419ffc4371f5c1d4a7f0e14ca5c116db84
Alex Plasencia <aPlasencia895@hotmail.com>
2022-02-15 11:57:44 -0800

    Completed the submit events for the journal entry form and verified that each new entry receives an incrementing entryId.

--- js/main.js
+++ js/main.js
@@ -9,12 +9,44 @@ function photoChange(event) {
 }
 
 function submitEntryForm(event) {
+  event.preventDefault();
+  var titleValue = $createEntry.elements.title.value;
+  var photoUrlValue = $createEntry.elements.photoUrl.value;
+  var notesValue = $createEntry.elements.notes.value;
 
+  var newEntryObject = {
+    title: titleValue,
+    photoUrl: photoUrlValue,
+    notes: notesValue
+  };
+
+  newEntryObject.EntryId = data.nextEntryId;
+  data.entries.unshift(newEntryObject);
+  data.nextEntryId++;
+  // console.log('the value of the data object is', data);
+  $img.src = 'images/placeholder-image-square.jpg';
+  $createEntry.reset();
 }
 
-$createEntry.addEventListener('input', submitEntryForm);
+$createEntry.addEventListener('submit', submitEntryForm);
 $photoUrl.addEventListener('input', photoChange);
+
+// var data = {
+//   view: 'entry-form',
+//   entries: [],
+//   editing: null,
+//   nextEntryId: 1
+// };
+
 // console.log('the value of $img is: ', $img.src);
 // var photoUrlValue = $createEntry.elements.photoUrl.value;
 // console.log('the value of the photourlvalue is: ', photoUrlValue);
 // console.log('the value of $createEntry is:', $createEntry);
+// console.log('the value of entryfromdata is: ', entryFromData);
+// console.log('the array is here', data.entries);
+// The console logs below worked in the submitEntryForm function properly
+// console.log('the value of titlevalue: is ', titleValue);
+// console.log('the value of photoUrlValue is: ', photoUrlValue);
+// console.log('the value of notesValue is: ', notesValue);
+// console.log('the value of newEntryObject is', newEntryObject);
+// console.log('the value of newentry object is: ', newEntryObject);
