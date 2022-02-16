682ed1f0794bdfb99a444c0fb4886eebbd9e0eb8
Alex Plasencia <aPlasencia895@hotmail.com>
2022-02-16 13:07:21 -0800

    Defined a function that takes a single journal entry object and returns a DOM tree that matches one of the example entries in the HTML.

--- js/data.js
+++ js/data.js
@@ -7,6 +7,46 @@ var data = {
   nextEntryId: 1
 };
 
+function renderEntries(dataEntries) {
+  var listedElement = document.createElement('li');
+
+  var divRow = document.createElement('div');
+  divRow.setAttribute('class', 'row');
+  listedElement.appendChild(divRow);
+
+  var divColumn1 = document.createElement('div');
+  divColumn1.setAttribute('class', 'column-half');
+  divRow.appendChild(divColumn1);
+
+  var entryImg = document.createElement('img');
+  entryImg.setAttribute('src', dataEntries.photoUrl);
+  entryImg.setAttribute('class', 'entry-image');
+  divColumn1.appendChild(entryImg);
+
+  var divColumn2 = document.createElement('div');
+  divColumn2.setAttribute('class', 'column-half');
+  divRow.appendChild(divColumn2);
+
+  var h1Title = document.createElement('h1');
+  h1Title.textContent = dataEntries.title;
+  divColumn2.appendChild(h1Title);
+
+  var pNotes = document.createElement('p');
+  pNotes.textContent = dataEntries.notes;
+  divColumn2.appendChild(pNotes);
+
+  return listedElement;
+}
+
+var $ul = document.querySelector('ul');
+// console.log('the value of $ul is ', $ul);
+// queried $ul returns the unordered list as intended
+
+for (var i = 0; i < data.entries.length; i++) {
+  var renderEntriesAppear = renderEntries(data[i]);
+  $ul.appendChild(renderEntriesAppear);
+}
+
 function grabLocalStorage(event) {
   var dataJSON = JSON.stringify(data);
   localStorage.setItem('cj-entry', dataJSON);
