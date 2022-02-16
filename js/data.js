71b171e56d505abcc27d2a22e635103aacbe18d3
Alex Plasencia <aPlasencia895@hotmail.com>
2022-02-15 17:14:20 -0800

    Moved the local storage related functions variables and event listeners and put them in js/data where they belong.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,16 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+function grabLocalStorage(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('cj-entry', dataJSON);
+}
+
+var previousDataJSON = localStorage.getItem('cj-entry');
+if (previousDataJSON !== null) {
+  previousDataJSON = JSON.parse(previousDataJSON);
+  data.entries = previousDataJSON.entries;
+  data.nextEntryId = previousDataJSON.nextEntryId;
+}
+window.addEventListener('beforeunload', grabLocalStorage);
--- js/main.js
+++ js/main.js
@@ -28,18 +28,5 @@ function submitEntryForm(event) {
   $createEntry.reset();
 }
 
-function grabLocalStorage(event) {
-  var dataJSON = JSON.stringify(data);
-  localStorage.setItem('cj-entry', dataJSON);
-}
-
-var previousDataJSON = localStorage.getItem('cj-entry');
-if (previousDataJSON !== null) {
-  previousDataJSON = JSON.parse(previousDataJSON);
-  data.entries = previousDataJSON.entries;
-  data.nextEntryId = previousDataJSON.nextEntryId;
-}
-
 $createEntry.addEventListener('submit', submitEntryForm);
 $photoUrl.addEventListener('input', photoChange);
-window.addEventListener('beforeunload', grabLocalStorage);
