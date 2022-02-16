94ad1944063d218bcad20670c11fcea777bc4575
Alex Plasencia <aPlasencia895@hotmail.com>
2022-02-15 16:42:34 -0800

    Completed User can create an entry issue-1.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 var $createEntry = document.querySelector('#create-entry');
 var $photoUrl = document.querySelector('#photo-entry');
 var $img = document.querySelector('img');
+
 function photoChange(event) {
   $img.src = event.target.value;
 }
@@ -23,30 +24,22 @@ function submitEntryForm(event) {
   newEntryObject.EntryId = data.nextEntryId;
   data.entries.unshift(newEntryObject);
   data.nextEntryId++;
-  // console.log('the value of the data object is', data);
   $img.src = 'images/placeholder-image-square.jpg';
   $createEntry.reset();
 }
 
+function grabLocalStorage(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('cj-entry', dataJSON);
+}
+
+var previousDataJSON = localStorage.getItem('cj-entry');
+if (previousDataJSON !== null) {
+  previousDataJSON = JSON.parse(previousDataJSON);
+  data.entries = previousDataJSON.entries;
+  data.nextEntryId = previousDataJSON.nextEntryId;
+}
+
 $createEntry.addEventListener('submit', submitEntryForm);
 $photoUrl.addEventListener('input', photoChange);
-
-// var data = {
-//   view: 'entry-form',
-//   entries: [],
-//   editing: null,
-//   nextEntryId: 1
-// };
-
-// console.log('the value of $img is: ', $img.src);
-// var photoUrlValue = $createEntry.elements.photoUrl.value;
-// console.log('the value of the photourlvalue is: ', photoUrlValue);
-// console.log('the value of $createEntry is:', $createEntry);
-// console.log('the value of entryfromdata is: ', entryFromData);
-// console.log('the array is here', data.entries);
-// The console logs below worked in the submitEntryForm function properly
-// console.log('the value of titlevalue: is ', titleValue);
-// console.log('the value of photoUrlValue is: ', photoUrlValue);
-// console.log('the value of notesValue is: ', notesValue);
-// console.log('the value of newEntryObject is', newEntryObject);
-// console.log('the value of newentry object is: ', newEntryObject);
+window.addEventListener('beforeunload', grabLocalStorage);
